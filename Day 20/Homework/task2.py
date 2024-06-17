49df7ebf7e56db034b55428124a4466caa548475
sandrolomadze <decmakusa@gmail.com>
2024-06-17 22:32:50 +0400

    commit

---
+++ Day 20/Homework/task2.py
@@ -0,0 +1,7 @@
+""" იმუშავეთ მონაცემების გადაქცევაზე """
+
+age = input("enter your age: ")
+height = input("enter your height: ")
+ahi = int(age)*float(height)
+print(type(ahi))
+print("your age_height_index is "+str(ahi))
