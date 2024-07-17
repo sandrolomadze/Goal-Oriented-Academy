c1d97d203642c9445664e4050cf2e53d0bfe8d92
sandrolomadze <decmakusa@gmail.com>
2024-07-17 21:44:47 +0400

    commit

---
+++ Day 22/Classwork/task1.py
@@ -0,0 +1,23 @@
+"""უნდა შექმნათ 10 სხვადასხვა მონაცემთა ტიპის ცვლადი და დაბეჭდოთ ისინი, 
+და დაბეჭდოთ მათი მონაცემთა ტიპები type() ფუნქციის გამოყენებით"""
+
+miqautaze=3
+print(type(miqautaze))
+kvara=1
+print(type(kvara))
+mamardashvili=32
+print(type(mamardashvili))
+bombarderi="miqautaze"
+print(type(bombarderi))
+ronaldo=3
+print(type(ronaldo))
+candy="kitkat"
+print(type(candy))
+car="lambo"
+print(type(car))
+country="georgia"
+print(type(country))
+day=False
+print(type(day))
+weight=7.7
+print(type(weight))
---
+++ Day 24/Homwork/task1.py
@@ -0,0 +1,7 @@
+"""დავალება: მომხმარებელს შემოატანინეთ 2 სახელი და შემდეგ შეადარეთ, თე სახელები უდრიან 
+ერთმანეთს მაშინ დაპრინტეთ True, ხოლო თუ არ უდრიან დაპრინტეთ False. print ფუნქცით გაკეთეთ"""
+
+name1=input("enter your first name: ")
+name2=input("enter your second name: ")
+
+print(name1==name2)
